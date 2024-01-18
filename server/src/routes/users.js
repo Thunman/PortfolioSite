59cf45553b1de5f341bf1601e0a84b5cd5a30d68
thunman <thunman42@gmail.com>
2024-01-18 17:59:10 +0100

    adding saveHighScore api

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,5 +9,6 @@ userRouter.post("/register", userController.registerUser);
 userRouter.post("/login", userController.loginUser);
 userRouter.get("/totalUsers", serverController.totalUsers);
 userRouter.put("/saveGameState",authMiddleware.verifyToken, userController.saveGameState)
+userRouter.put("/saveHighScore", authMiddleware.verifyToken, userController.saveScore);
 
 export default userRouter;
