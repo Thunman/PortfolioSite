ef2dc17f0c5604e7993e4484468c5fb8ea6db5c9
thunman <thunman42@gmail.com>
2024-01-18 12:58:54 +0100

    fix

--- server/src/model/userModel.js
+++ server/src/model/userModel.js
@@ -1,40 +1,51 @@
 import mongoose from "mongoose";
 
+const highScoreSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
+  score: {
+    type: Number,
+  },
+});
+
 const userSchema = new mongoose.Schema({
 
-    userName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    isAdmin: {type: Boolean, default: false},
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: { type: Boolean, default: false },
 
-    gameState: {
-        score: { type: Number, default: 0 },
-        timeLeft: { type: Number, default: 0 },
-        circles: [
-          {
-            color: { type: String, default: "" },
-            id: { type: Number, default: 0 },
-            top: { type: String, default: "" },
-            left: { type: String, default: "" },
-          },
-        ],
+  gameState: {
+    score: { type: Number, default: 0 },
+    timeLeft: { type: Number, default: 0 },
+    circles: [
+      {
+        color: { type: String, default: "" },
+        id: { type: Number, default: 0 },
+        top: { type: String, default: "" },
+        left: { type: String, default: "" },
       },
-    
-    
-}, {timestamps: true});
+    ],
+  },
 
+
+
+}, { timestamps: true });
+
+const HighScore = mongoose.model("highScore", highScoreSchema);
 const User = mongoose.model("user", userSchema);
 
-export default User;
+export {User, HighScore};
